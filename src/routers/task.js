68b00f8064f40660d6cdeceae5c89765e0b35b79
Emircan Kucukmotor <emircan.kucukmotor92@gmail.com>
2021-08-15 19:42:07 +0300

    Authenticated task endpoints.

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,20 +17,20 @@ router.post('/tasks', auth, async (req, res) => {
     }
 })
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({});
-        res.send(tasks);
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks);
     } catch (e) {
         res.status(500).send();
     }
 })
 
-router.get('/tasks/:id', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
 
     try {
-        const task = await Task.findById(_id);
+        const task = await Task.findOne({ _id, owner: req.user._id })
 
         if (!task) {
             return res.status(404).send();
@@ -42,7 +42,7 @@ router.get('/tasks/:id', async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdates = ['description', 'completed'];
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
@@ -54,7 +54,7 @@ router.patch('/tasks/:id', async (req, res) => {
     const _id = req.params.id;
 
     try {
-        const task = await Task.findById(_id);
+        const task = await Task.findOne({ _id, owner: req.user._id })
         
         if (!task) {
             return res.status(404).send();
@@ -69,11 +69,11 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     const _id = req.params.id;
 
     try {
-        const task = await Task.findByIdAndDelete(_id);
+        const task = await Task.findOneAndDelete({ _id, owner: req.user._id });
 
         if (!task) {
             return res.status(404).send();
