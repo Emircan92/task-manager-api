1b03c62afbe58d5bbb41464f395918fc32633483
Emircan Kucukmotor <emircan.kucukmotor92@gmail.com>
2021-09-12 15:39:24 +0300

    swagger init and cors allow all.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 require('./db/mongoose');
+const swaggerUi = require('swagger-ui-express');
+// const swaggerDocument = require('./swagger.json');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
 const app = express()
 
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 app.use(taskRouter);
 
