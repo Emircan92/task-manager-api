bec20d50358a4e75ecde404482542c86f2d0463c
Emircan Kucukmotor <47698328+Emircan92@users.noreply.github.com>
2021-09-16 14:24:55 +0300

    cors options param.

--- src/app.js
+++ src/app.js
@@ -4,8 +4,16 @@ const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
 const app = express()
-
 app.use(express.json());
+
+var options = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }
+
+app.use(cors(options));
 app.use(userRouter);
 app.use(taskRouter);
 
